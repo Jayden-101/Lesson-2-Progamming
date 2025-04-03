d2f99b90ed294be1a42829729378a34ccfe3343b
Jayden-101 <jaydenmaxmillian2@gmail.com>
2025-04-03 17:12:19 +1300

    Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Creating different variables
+
+x = 5
+y = "John"
+print (x)
+print (y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+
+print("\n Hello", name, "\nwelcome to codingal")
