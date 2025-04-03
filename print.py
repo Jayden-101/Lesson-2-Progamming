9451e22c2b7748bbf244808ddb2bb1a98f1034e0
Jayden-101 <jaydenmaxmillian2@gmail.com>
2025-04-03 16:58:36 +1300

    Printing

---
+++ print.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a Numner
+print(4)
+
+#Next Line after print
+print("Hello Python /n")
+
+#Pass multiple arguements for printing
+print("hello",5)
+
+#End agruement of Print Statement
+print("welcome to ", end= "$")
